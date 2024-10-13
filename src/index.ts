e254288a1a28811c5b2af78f52324a7e102bb068
bartoszkocylo <bartoszk236@gmail.com>
2024-10-13 20:32:47 +0200

    task 2 done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import OpenAI from "openai";
 import dotenv from "dotenv";
 import fs from "fs"
+import { mkdir } from 'fs';
+import * as path from 'path';
 
 dotenv.config(); // Load environment variables
 
@@ -11,7 +13,7 @@ const openai = new OpenAI({
 
 const systemPrompt: string = `Jesteś odgadywaczem kontekstu tekstu. Twoim zadaniem jest powiedzieć na podstawie tekstu, o czym jest.`
 
-async function  summariseText (text: string) {
+async function summariseText (text: string) {
     try {
         const response =  await openai.chat.completions.create({
             model: "gpt-4o-mini",
@@ -28,16 +30,39 @@ async function  summariseText (text: string) {
     catch (error) {
         throw new Error("Error in openAI Response");
     }
-
 }
 
-const main = async () => {
-    const file = await fs.promises.readFile("src/request.txt", "utf8");
-    const response = await summariseText(file);
-    await fs.promises.writeFile("response.txt", response, "utf8");
+async function createDirectory(){
+    mkdir('responses', { recursive: true }, (err) => {
+        if (err) {
+            console.error('Błąd przy tworzeniu katalogu:', err);
+        } else {
+            console.log('Katalog został pomyślnie utworzony.');
+        }
+    });
+}
+
+async function main() {
+    const directoryPath = `src/requests`;
+    await createDirectory();
+    try {
+        const files = await fs.promises.readdir(directoryPath);
+
+        for (const file of files) {
+
+            const filePath = path.join(directoryPath, file);
+
+            if (path.extname(file) === '.txt') {
+                const fileString = await fs.promises.readFile(`${filePath}`, "utf8");
+                const response = await summariseText(fileString);
+
+                const baseName = path.basename(file, '.txt');
+                await fs.promises.writeFile(`responses/${baseName}_summary.txt`, response, "utf8");
+            }
+        }
+    } catch (err) {
+        console.error('Wystąpił błąd przy odczytywaniu folderu:', err);
+    }
 }
 
 main().then(() => console.log("Done"));
-
-
-
